8ea6caaf29e6af573d3be1a4bc802826d7372ed9
Jérémie Astori <jeremie@astori.fr>
2018-02-08 00:19:44 -0500

    Load packages from package.json, fix loading thelounge metadata from the wrong file, add tests

---
+++ test/plugins/packages/indexTest.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const expect = require("chai").expect;
+const TestUtil = require("../../util");
+
+let packages;
+
+describe("packages", function() {
+	let originalLogInfo;
+
+	beforeEach(function() {
+		originalLogInfo = log.info;
+		log.info = () => {};
+	});
+
+	afterEach(function(done) {
+		setTimeout(() => {
+			log.info = originalLogInfo;
+			done();
+		});
+	});
+
+	beforeEach(function() {
+		delete require.cache[require.resolve("../../../src/plugins/packages")];
+		packages = require("../../../src/plugins/packages");
+	});
+
+	describe(".getStylesheets", function() {
+		it("should contain no stylesheets before packages are loaded", function() {
+			expect(packages.getStylesheets()).to.be.empty;
+		});
+
+		it("should return the list of registered stylesheets for loaded packages", function() {
+			packages.loadPackages();
+
+			expect(packages.getStylesheets()).to.deep.equal([
+				"thelounge-package-foo/style.css",
+			]);
+		});
+	});
+
+	describe(".getPackage", function() {
+		it("should contain no reference to packages before loading them", function() {
+			expect(packages.getPackage("thelounge-package-foo")).to.be.undefined;
+		});
+
+		it("should return details of a registered package after it was loaded", function() {
+			packages.loadPackages();
+
+			expect(packages.getPackage("thelounge-package-foo"))
+				.to.have.key("onServerStart");
+		});
+	});
+
+	describe(".loadPackages", function() {
+		it("should display report about loading packages", function(done) {
+			// Mock `log.info` to extract its effect into an array
+			const stdout = [];
+			log.info = TestUtil.mockLogger((str) => stdout.push(str));
+
+			packages.loadPackages();
+
+			setTimeout(() => {
+				expect(stdout).to.deep.equal(["Package thelounge-package-foo loaded\n"]);
+				done();
+			});
+		});
+	});
+});
